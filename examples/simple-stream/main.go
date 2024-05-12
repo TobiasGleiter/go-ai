461feff641b7feee1b1e88af9a37a859cc42a3a7
Tobias Gleiter <gleiter.tobias@outlook.de>
2024-05-12 11:13:09 +0200

    refactor: ollama model struct

--- examples/simple-stream/main.go
+++ examples/simple-stream/main.go
@@ -6,7 +6,7 @@ import (
 
 func main() {
 	prompt := "Marty has 100 centimeters of ribbon that he must cut into 4 equal parts. Each of the cut parts must be divided into 5 equal parts. How long will each final cut be?"
-
+	
 	// Streams the response and output in the console.
 	ollama.GenerateSingleStream(prompt)
 }
